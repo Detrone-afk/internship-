ea2eb5b4fed64c374ec49587eac801be95175f3d
Detrone-afk <221210023@nitdelhi.ac.in>
2025-05-19 22:39:19 +0530

    fix: errors

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 
 export default function DarkModeToggle() {
-  const [, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem("theme") === "dark") {
@@ -11,7 +11,7 @@ export default function DarkModeToggle() {
     }
   }, []);
 
-  {/*const toggleDarkMode = () => {
+  const toggleDarkMode = () => {
     if (darkMode) {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
@@ -23,16 +23,11 @@ export default function DarkModeToggle() {
   };
 
   {/*return (
-    
-    <div className="flex justify-between items-center w-full p-3">
-      <Image src="/detrone.png" alt="DETRONE Logo" width={120} height={40} />
-
-      <button
-        onClick={toggleDarkMode}
-        className="p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded-md"
-      >
-        {darkMode ? "🌙 Dark Mode" : "☀️ Light Mode"}
-      </button>
-    </div>
+    <button
+      onClick={toggleDarkMode}
+      className="p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded-md"
+    >
+      {darkMode ? "🌙 Dark Mode" : "☀️ Light Mode"}
+    </button>
   );*/}
 }
