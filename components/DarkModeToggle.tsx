8b4e4d0018d3f3e1cf2512e97f8115f6e4beb7fb
Detrone-afk <221210023@nitdelhi.ac.in>
2025-05-19 22:43:03 +0530

    fix: errors

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -1,33 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function DarkModeToggle() {
-  const [darkMode, setDarkMode] = useState(false);
-
   useEffect(() => {
     if (localStorage.getItem("theme") === "dark") {
       document.documentElement.classList.add("dark");
-      setDarkMode(true);
     }
   }, []);
 
-  const toggleDarkMode = () => {
-    if (darkMode) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    }
-    setDarkMode(!darkMode);
-  };
-
-  {/*return (
-    <button
-      onClick={toggleDarkMode}
-      className="p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded-md"
-    >
-      {darkMode ? "🌙 Dark Mode" : "☀️ Light Mode"}
-    </button>
-  );*/}
+  return null;
 }
